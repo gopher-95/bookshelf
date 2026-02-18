d69bdaffd8deb364eb4bb1cb863f228f1404d954
gopher-95 <alexander.dobrov@iss.ru>
2026-02-18 03:44:42 +0300

    added two handlers

--- cmd/server/routes.go
+++ cmd/server/routes.go
@@ -8,4 +8,6 @@ import (
 func setupRoutes(r *chi.Mux) {
 	r.Post("/add", api.AddBookHandler)
 	r.Get("/books", api.GetAllBooks)
+	r.Get("/books/{id}", api.GetBook)
+	r.Delete("/books/{id}", api.DeleteBook)
 }
